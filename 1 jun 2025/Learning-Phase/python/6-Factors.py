4f65f77979ee734ec7927df85a96eefb79110901
hirsheel <dusk.apex.10@gmail.com>
2025-06-12 19:38:57 +0530

    comments added

--- 1 jun 2025/Learning-Phase/python/6-Factors.py
+++ 1 jun 2025/Learning-Phase/python/6-Factors.py
@@ -1,17 +1,17 @@
 print("This Program is to Get All The Factors of given number")
 num = input("Enter Your Integer: ")
-try:
+try:  # checks whether its a number or string
     flo = float(num)
-    if flo.is_integer():
+    if flo.is_integer():  # checks whether float is integer
         flo = int(flo)
         i = 1
         fac = []
-        while i <= flo:
+        while i <= flo: # check for divisors
             if flo % i == 0:
-                fac.append(str(i))
+                fac.append(str(i))  
             i += 1
         print('Factors: ', ", ".join(fac))
-    else:
+    else:  # executes when float is a decimal
         print("Give a Valid Integral Input please!")
-except:
+except:  # executes when input isnt a number
     print("Give a Valid Integer!")
