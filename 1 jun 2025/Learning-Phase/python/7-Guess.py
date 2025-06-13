5e25e5925b9a30b7c03b42e0ae4a5d93407706e9
hirsheel <dusk.apex.10@gmail.com>
2025-06-13 16:44:09 +0530

    Working Guessing Game

--- 1 jun 2025/Learning-Phase/python/7-Guess.py
+++ 1 jun 2025/Learning-Phase/python/7-Guess.py
@@ -1,5 +1,27 @@
-ask = input("Enter your word: ")
-if ask == ask[::-1]:
-    print("its the same")
-else: 
-    print("nah")
+import random 
+num = random.randint(1, 100)
+print("Welcome to Number Guessing Game!")
+print("How to play: A random number is choosed between 1-100 and hints are given to guess. \nIf user Fails to guess it in 15 times, You LOSE!")
+i=1
+while i <= 15:
+    guess = input(f"Enter Your Guess(chance {i}): ")
+    try:
+        guess = float(guess)
+        if guess <= 100 and guess >= 1:
+            if guess < num:
+                print("Guess Higher Value...")
+            elif guess > num:
+                print("Guess Lowerer...")
+            elif guess == num:
+                print("THATS THE EXACT NUMBER!")
+                print("Congratulations You Won!")
+                break
+        else:
+            print("Guess a number between 1-100 (1 and 100 included)")
+    except:
+        if guess.uppper == "Q":
+            print("Thanks for playing")
+            break
+        else:
+            print("Give a Valid Number...")
+    i += 1
