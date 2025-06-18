71d7bc7e667c9c4f16f9b6d681ce258194908ef0
hirsheel <dusk.apex.10@gmail.com>
2025-06-18 17:57:24 +0530

    Added 3rd section

--- 1 jun 2025/Learning-Phase/python/8-To_do_List/8-To_do_List.py
+++ 1 jun 2025/Learning-Phase/python/8-To_do_List/8-To_do_List.py
@@ -1,20 +1,67 @@
-print("Choose from The Options Below:")
-print("1.Add tasks")
-print("2.Delete tasks")
-print("3.Mark tasks as complete")
-print("4.Remark Completed Tasks as Incomplete")
-print("5.View all incomplete tasks")
-print("6.Show all completed tasks")
-ask = input("Choose Your Option(1-6) or q to Quit: ").strip()
-if ask.lower() == "q":
-    print("Have A Nice Day!")
-else:
-    try:
-        ask = int(ask)
-        if ask == 1:
-            task = input("State Your Task: ").strip()
-            with open("Task.txt", "a") as a:
-                a.write(task + "\n")
-            
-    except:
-        print("Please Give A Valid Input (1-6) Or q to Quit")
+def open_and_print(task_):
+    print("\nHere is a List of Tasks:")
+    with open(task_, "r") as file:
+        lines = file.readlines()
+    for i, item in enumerate(lines):
+            print(f'{i+1}.{item}', end = "")
+while True:
+    print("Choose from The Options Below:\n")
+    print("1.Add tasks")
+    print("2.Update tasks")
+    print("3.Delete tasks")
+    print("4.Mark tasks as complete")
+    print("5.Remark Completed Tasks as Incomplete")
+    print("6.Show all tasks\n")
+    ask = input("Choose Your Option(1-6) or q to Quit: ").strip()
+    if ask.lower() == "q":
+        print("Have A Nice Day!")
+        break
+    else:
+        try:
+            ask = int(ask)
+            if ask == 1:
+                task = input("State Your Task: ").strip()
+                with open("Task.txt", "a") as a:
+                    a.write(task + "\n")
+                print("\n-----------Added-------------\n")
+            elif ask == 2:
+                open_and_print("Task.txt")
+                line_number = input("Enter Your Line to Change: ")
+                try:
+                    line_number = int(line_number)
+                    if 1 <= line_number <= len(lines):
+                        new_text = input("Enter Your Text: ")
+                        lines[line_number - 1] = new_text + "\n"
+                        with open("Task.txt", "w") as file:
+                            file.writelines(lines)
+                        print(f"\n------------Line {line_number} updated.---------------\n")
+                    else:
+                        print("\n---Invalid line number---\n")
+                except:
+                    print("\n-----PLease Choose A valid Line------\n")
+            elif ask == 3:
+                print("\nHere is a List of Tasks:")
+                with open("Task.txt", "r") as file:
+                    lines = file.readlines()
+                    for i, item in enumerate(lines):
+                        print(f'{i+1}.{item}', end = "")
+                line_number = input("Enter Your Line to Delete: ")
+                try:
+                    line_number = int(line_number)
+                    if 1 <= line_number <= len(lines):
+                        with open("Task.txt", "w") as file:
+                            del lines[line_number - 1]
+                            file.writelines(lines)
+                        print(f"\n------------Line {line_number} deleted---------------\n")
+                    else:
+                        print("\n---Invalid line number---\n")
+                except:
+                    print("\n-----PLease Choose A valid Line------\n")
+            elif ask == 4:
+                
+            elif ask == 5:
+            elif ask == 6:
+            else:
+                print("Please Give A Valid Input (1-6) Or q to Quit")   
+        except:
+            print("Please Give A Valid Input (1-6) Or q to Quit")
